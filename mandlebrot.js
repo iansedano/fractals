3c8bf976dc7a3cfccd4c45b4bd47b548a374e9d4
iansedano <iansedano@gmail.com>
2020-05-15 11:55:17 +0200

    Updated from Daniel Schiffman

--- mandlebrot.js
+++ mandlebrot.js
@@ -1,48 +1,68 @@
-let theta;
+
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/6z7GQewK-Ks
+
+var minval = -0.5;
+var maxval = 0.5;
+
+var minSlider;
+var maxSlider;
+
+var frDiv;
 
 function setup() {
-  createCanvas(710, 400);
+  createCanvas(500, 500);
+  pixelDensity(1);
+
+  minSlider = createSlider(-2.5, 0, -2.5, 0.01);
+  maxSlider = createSlider(0, 2.5, 2.5, 0.01);
+
+  frDiv = createDiv('');
 }
 
 function draw() {
-  background(0);
-  frameRate(30);
-  stroke(255);
-  // Let's pick an angle 0 to 90 degrees based on the mouse position
-  let a = (mouseX / width) * 360;
-  // Convert it to radians
-  theta = radians(a);
-  // Start the tree from the bottom of the screen
-  translate(width/2,height);
-  // Draw a line 120 pixels
-  line(0,0,0,-120);
-  // Move to the end of that line
-  translate(0,-120);
-  // Start the recursive branching!
-  branch(120);
+  var maxiterations = 100;
 
-}
+  loadPixels();
+  for (var x = 0; x < width; x++) {
+    for (var y = 0; y < height; y++) {
 
-function branch(h) {
-  // each branch proportion of previous one
-  h *= 0.7;
+      var a = map(x, 0, width, minSlider.value(), maxSlider.value());
+      var b = map(y, 0, height, minSlider.value(), maxSlider.value());
 
-  // All recursive functions must have an exit condition!!!!
-  // Here, ours is when the length of the branch is 2 pixels or less
-  if (h > 3) {
-    push();    // Save the current state of transformation (i.e. where are we now)
-    rotate(theta);   // Rotate by theta
-    line(0, 0, 0, -h);  // Draw the branch
-    translate(0, -h); // Move to the end of the branch
-    branch(h);       // Ok, now call myself to draw two new branches!!
-    pop();     // Whenever we get back here, we "pop" in order to restore the previous matrix state
+      var ca = a;
+      var cb = b;
 
-    // Repeat the same thing, only branch off to the "left" this time!
-    push();
-    rotate(-theta);
-    line(0, 0, 0, -h);
-    translate(0, -h);
-    branch(h);
-    pop();
+      var n = 0;
+
+      while (n < maxiterations) {
+        var aa = a * a - b * b;
+        var bb = 2 * a * b;
+        a = aa + ca;
+        b = bb + cb;
+        if (a * a + b * b > 16) {
+          break;
+        }
+        n++;
+      }
+
+      var bright = map(n, 0, maxiterations, 0, 1);
+      bright = map(sqrt(bright), 0, 1, 0, 255);
+
+      if (n == maxiterations) {
+        bright = 0;
+      }
+
+      var pix = (x + y * width) * 4;
+      pixels[pix + 0] = bright;
+      pixels[pix + 1] = bright;
+      pixels[pix + 2] = bright;
+      pixels[pix + 3] = 255;
+    }
   }
+  updatePixels();
+
+  frDiv.html(floor(frameRate()));
 }
